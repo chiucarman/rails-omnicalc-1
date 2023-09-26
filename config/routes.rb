8519d13df670511f4c5f0cd73ffeba4066632a0e
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-26 21:09:53 +0000

    main square page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  
+#  homepage
+  get("/", { :controller => "forms", :action => "square"})
+
+#  square
+ get("/square/new", { :controller => "forms", :action => "square"})
+#  get("/square/results", { :controller => "forms", :action => "square_results"})
+
+# #  square_root
+#  get("/square_root/new", { :controller => "forms", :action => "square_root"})
+
+# #  payment
+#  get("/payment/new", { :controller => "forms", :action => "payment"})
 end
